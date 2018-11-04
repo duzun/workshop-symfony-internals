1ec6eec672ecedaa6d63c37bf2da2a67f04ceae1
Dumitru Uzun <contact@duzun.me>
2018-11-04 11:58:48 +0200

    HTTP objects

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7\Response;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
+use Nyholm\Psr7Server\ServerRequestCreator;
 
-if (isset($_GET['page']) && $_GET['page'] === 'foo') {
-    echo "Foo page <br>";
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$psr17Factory = new Psr17Factory();
+
+$creator = new ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
+
+$request = $creator->fromGlobals();
+
+if ( isset($request->getQueryParams()['page']) && $request->getQueryParams()['page'] === 'foo') {
+	$response = new Response('200', [], 'Foo page <br>');
 } else {
-    echo "Welcome to index! <br>";
+	$response = new Response('200', [], "Welcome to index! <br>");
 }
 
-if ($_SERVER['REMOTE_ADDR'] === '127.0.0.1') {
-    echo "(admin stuff)";
+if ( $request->getServerParams()['REMOTE_ADDR'] === '127.0.0.1') {
+	$response = new Response('200', [], $response->getBody() . "(admin stuff)");
 }
+
+(new SapiEmitter())->emit($response);
